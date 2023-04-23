9965d1bed1924b202a43577d81503930ad3f8d37
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-04-23 20:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function calculateMinCost(event) {
-event.preventDefault();
+function calculateMinCost() {
+// event.preventDefault();
   var ans = document.querySelector("input").value;
   var updated = ans.split(",");
 
